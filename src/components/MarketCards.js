65d2f4a6cc79d25bc82245f4c0bf5efe84cdd662
Pragnan143 <pragnanmanam8@gmail.com>
2024-07-12 13:56:52 +0530

    Marketing Hook section Done

--- src/components/MarketCards.js
+++ src/components/MarketCards.js
@@ -1,11 +1,31 @@
 import React from "react";
 const SmallCard = () => {
-  return <div className="w-[400px] h-[200px] bg-gray-500 rounded-2xl"></div>;
+  return (
+    <div className="w-[200px] h-[200px] bg-gray-500 rounded-2xl z-30">
+      <img src="" alt="" />
+    </div>
+  );
+};
+const BigCard = () => {
+  return (
+    <div className="w-[400px] h-[200px] bg-gray-500 rounded-2xl z-30">
+      <img src="" alt="" />
+    </div>
+  );
 };
 const MarketCards = () => {
   return (
-    <div>
+    <div className="mx-24 my-10 flex flex-wrap gap-10 ">
+      <BigCard />
+      <SmallCard /> <SmallCard />
+      <BigCard />
       <SmallCard />
+      <BigCard />
+      <SmallCard />
+      <SmallCard />
+      <SmallCard />
+      <SmallCard /> <BigCard />
+      <BigCard />
     </div>
   );
 };
